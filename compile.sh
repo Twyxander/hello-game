54e335182d5cc77080fb224299960ffd47c708d2
Jan Vlček <honza.vlcku@gmail.com>
2019-12-24 10:02:07 +0100

    Přidali jsme druhého hráče

--- compile.sh
+++ compile.sh
@@ -1,1 +1,6 @@
-gcc -o hello hello.c -lallegro -lallegro_main -lallegro_font -lallegro_primitives -lallegro_ttf
+gcc -o hello hello.c \
+    -lallegro -lallegro_main \
+    -lallegro_font \
+    -lallegro_image \
+    -lallegro_primitives \
+    -lallegro_ttf
